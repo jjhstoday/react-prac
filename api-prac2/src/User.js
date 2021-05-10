0cbfb7ee365198837b8a56be79f22d4b42f7be50
jjhstoday <jjh.s.today@gmail.com>
2021-05-10 18:43:21 +0900

    feat: 리액트 api 연습

---
+++ api-prac2/src/User.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useUsersState, useUsersDispatch, getUser } from './UsersContext';
+
+export default function User({ id }) {
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+
+  useEffect(() => {
+    getUser(dispatch, id);
+  }, [dispatch, id]);
+
+  const { loading, data: user, error } = state.user;
+
+  if (loading) return <div>로딩중</div>;
+  if (error) return <div>에러 발생</div>;
+  if (!user) return null;
+
+  return (
+    <div>
+      <h2>{user.username}</h2>
+      <p>
+        <b>Email: </b> {user.email}
+      </p>
+    </div>
+  );
+}
