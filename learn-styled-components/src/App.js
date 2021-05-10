177ae4b15306f9adb992b4380359a774192a8893
jjhstoday <jjh.s.today@gmail.com>
2021-05-10 18:45:05 +0900

    feat: styled-components 연습

---
+++ learn-styled-components/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import Button from './components/Button';
+import Dialog from './components/Dialog';
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const palette = {
+  blue: '#228be6',
+  gray: '#496057',
+  pink: '#f06595'
+};
+
+function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => setDialog(true);
+  const onConfirm = () => setDialog(false);
+  const onCancel = () => setDialog(false);
+  return (
+    <ThemeProvider
+      theme={{
+        palette
+      }}
+    >
+      <>
+        <AppBlock>
+          <Button onClick={onClick}>삭제</Button>
+        </AppBlock>
+        <Dialog
+          title='정말로 삭제하시겠습니까?'
+          confirmText='삭제'
+          cancelText='취소'
+          visible={dialog}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
