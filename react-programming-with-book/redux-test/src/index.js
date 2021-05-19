fe5cee582b60c31c1d3444d62178393595e539f4
jjhstoday <jjh.s.today@gmail.com>
2021-05-20 00:40:42 +0900

    feat: 여러 리듀서 하나로 합치기

--- react-programming-with-book/redux-test/src/index.js
+++ react-programming-with-book/redux-test/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers } from 'redux';
+import timeLineReducer, {
+  addTimeline,
+  removeTimeline,
+  editTimeline,
+  increaseNextPage
+} from './timeline/state';
+import friendReducer, {
+  addFriend,
+  removeFriend,
+  editFriend
+} from './friend/state';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = combineReducers({
+  timeline: timeLineReducer,
+  friend: friendReducer
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+store.subscribe(() => {
+  const state = store.getState();
+  console.log(state);
+});
+
+store.dispatch(addTimeline({ id: 1, desc: '고양이는 정말 귀여워' }));
+store.dispatch(addTimeline({ id: 2, desc: '강아지는 정말 귀여워' }));
+store.dispatch(increaseNextPage());
+store.dispatch(editTimeline({ id: 2, desc: '강아지는 정말정말*999 귀여워' }));
+store.dispatch(removeTimeline({ id: 1, desc: '고양이는 정말 귀여워' }));
+
+store.dispatch(addFriend({ id: 1, name: '정국' }));
+store.dispatch(addFriend({ id: 2, name: '뷔' }));
+store.dispatch(editFriend({ id: 2, name: '태형' }));
+store.dispatch(removeFriend({ id: 1, name: '정국' }));
