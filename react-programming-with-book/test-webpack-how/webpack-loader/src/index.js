8ce036516d55cc290a9cac66cd68c23eee1fdcd3
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 00:17:46 +0900

    feat: webpack에 babel-loader 설정 실습

---
+++ react-programming-with-book/test-webpack-how/webpack-loader/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+  return (
+    <div className='container'>
+      <h3 className='title'>webpack example</h3>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
