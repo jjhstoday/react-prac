ebc68c0eade04805ead1e6fa5e923842f87d98af
jjhstoday <jjh.s.today@gmail.com>
2021-05-20 11:15:27 +0900

    refactor: react-redux 코드 수정

--- react-programming-with-book/redux-test/src/friend/containers/FriendMain.js
+++ react-programming-with-book/redux-test/src/friend/containers/FriendMain.js
@@ -1,31 +1,18 @@
-import React, { useReducer, useEffect } from 'react';
-import store from '../../common/store';
+import React from 'react';
 import { getNextFriend } from '../../common/mockData';
 import { addFriend } from '../state';
 import FriendList from '../components/FriendList';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function FriendMain() {
-  const [, forceUpdate] = useReducer(v => v + 1, 0);
-
-  useEffect(() => {
-    let prevFriends = store.getState().friend.friends;
-    const unsubscribe = store.subscribe(() => {
-      const friends = store.getState().friend.friends;
-      if (prevFriends !== friends) {
-        forceUpdate();
-      }
-      prevFriends = friends;
-    });
-    return () => unsubscribe();
-  }, []);
+  const friends = useSelector(state => state.friend.friends);
+  const dispatch = useDispatch();
 
   function onAdd() {
     const friend = getNextFriend();
-    store.dispatch(addFriend(friend));
+    dispatch(addFriend(friend));
   }
   console.log('FriendMain render');
-
-  const friends = store.getState().friend.friends;
 
   return (
     <div>
--- react-programming-with-book/redux-test/src/index.js
+++ react-programming-with-book/redux-test/src/index.js
@@ -1,34 +1,16 @@
-import { createStore, combineReducers } from 'redux';
-import timelineReducer, {
-  addTimeline,
-  removeTimeline,
-  editTimeline,
-  increaseNextPage
-} from './timeline/state';
-import friendReducer, {
-  addFriend,
-  removeFriend,
-  editFriend
-} from './friend/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TimelineMain from './timeline/containers/TimelineMain';
+import FriendMain from './friend/containers/FriendMain';
+import store from './common/store';
+import { Provider } from 'react-redux';
 
-const reducer = combineReducers({
-  timeline: timelineReducer,
-  friend: friendReducer
-});
-
-const store = createStore(reducer);
-store.subscribe(() => {
-  const state = store.getState();
-  console.log(state);
-});
-
-store.dispatch(addTimeline({ id: 1, desc: '고양이는 정말 귀여워' }));
-store.dispatch(addTimeline({ id: 2, desc: '강아지는 정말 귀여워' }));
-store.dispatch(increaseNextPage());
-store.dispatch(editTimeline({ id: 2, desc: '강아지는 정말정말*999 귀여워' }));
-store.dispatch(removeTimeline({ id: 1, desc: '고양이는 정말 귀여워' }));
-
-store.dispatch(addFriend({ id: 1, name: '정국' }));
-store.dispatch(addFriend({ id: 2, name: '뷔' }));
-store.dispatch(editFriend({ id: 2, name: '태형' }));
-store.dispatch(removeFriend({ id: 1, name: '정국' }));
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <FriendMain />
+      <TimelineMain />
+    </div>
+  </Provider>,
+  document.getElementById('root')
+);
