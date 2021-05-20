204ea1d2459543a7067f7d4dd9e2c29d6f0c3f87
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 00:23:33 +0900

    feat: webpack에 css-loader 설정 실습

--- react-programming-with-book/test-webpack-how/webpack-loader/src/index.js
+++ react-programming-with-book/test-webpack-how/webpack-loader/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Style from './App.css';
 
 export default function App() {
+  console.log({ Style });
   return (
     <div className='container'>
       <h3 className='title'>webpack example</h3>
