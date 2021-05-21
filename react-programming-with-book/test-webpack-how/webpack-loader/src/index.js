5cea3d3feb7c327447e29adfd938fb439dbf38c9
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 14:07:43 +0900

    feat: 기타 파일 로더 실습

--- react-programming-with-book/test-webpack-how/webpack-loader/src/index.js
+++ react-programming-with-book/test-webpack-how/webpack-loader/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Style from './App.css';
+import Icon from './icon.png';
+import Json from './data.json';
+import Text from './data.txt';
 
 export default function App() {
   console.log({ Style });
   return (
     <div className='container'>
       <h3 className='title'>webpack example</h3>
+      <div>{`name: ${Json.name}, age: ${Json.age}`}</div>
+      <div>{`text: ${Text}`}</div>
+      <img src={Icon} alt='Icon' />
     </div>
   );
 }
