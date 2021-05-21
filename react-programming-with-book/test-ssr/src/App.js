58a1022dd1714ff036aa2f42919ec550bff4d040
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 22:15:33 +0900

    feat: 클라이언트에서만 렌더링 실습

---
+++ react-programming-with-book/test-ssr/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Home from './Home';
+import About from './About';
+
+export default function App({ defaultPage }) {
+  const [page, setPage] = useState(defaultPage);
+  useEffect(() => {
+    window.onpopstate = event => {
+      setPage(event.state);
+    };
+  }, []);
+
+  function onChangePage(e) {
+    const newPage = e.target.dataset.page;
+    window.history.pushState(newPage, '', `/${newPage}`);
+    setPage(newPage);
+  }
+
+  const PageComponent = page === 'home' ? Home : About;
+
+  return (
+    <div className='container'>
+      <button data-page='home' onClick={onChangePage}>
+        Home
+      </button>
+      <button data-page='about' onClick={onChangePage}>
+        About
+      </button>
+      <PageComponent />
+    </div>
+  );
+}
