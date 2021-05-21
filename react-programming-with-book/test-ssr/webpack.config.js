b18c51a022aabe639540559c4d1b21bd5e759e1c
jjhstoday <jjh.s.today@gmail.com>
2021-05-21 23:18:11 +0900

    feat: 서버 데이터 클라이언트로 전달

--- react-programming-with-book/test-ssr/webpack.config.js
+++ react-programming-with-book/test-ssr/webpack.config.js
@@ -5,13 +5,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            configFile: path.resolve(__dirname, '.babelrc.client.js')
+          }
+        }
       }
     ]
   },
