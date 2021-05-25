a7c3829e7558e4092ed89b5c5de74f4085ce527e
jjhstoday <jjh.s.today@gmail.com>
2021-05-25 23:07:55 +0900

    feat: express 활용한 서버 구축

--- react-programming-with-book/test-next/pages/page1.js
+++ react-programming-with-book/test-next/pages/page1.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import Icon from '../static/icon.png';
+import { add } from '../src/util';
 
 export default function Page1() {
   return (
     <div>
+      <p>{`10 + 20 = ${add(10, 20)}`}</p>
       <p>This is home page.</p>
       <img src={Icon} alt='' />
       <Head>
